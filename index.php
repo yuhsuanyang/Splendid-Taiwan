b4bec633715298477820bc5b861e4b5c15be73f1
yuhsuanyang <sb0953330882@yahoo.com.tw>
2018-07-10 19:26:07 +0800

    web project "Spendid Taiwan"

---
+++ index.php
@@ -0,0 +1,198 @@
+<?php
+
+$memorial_hall="chiang kai-shek memorial hall";
+$hsiangshan="hsianngshan";
+$taipei101="taipei 101";
+$beitou="beitou";
+$yehliu="yehliu";
+$jiufen="jiufen";
+$taimsui="taimsui";
+$yangmingshan="yangmingshan national park";
+//$shifen="waterfall";
+$houtong="houtong";
+$shilin="shilin night market";
+$pingxi="pingxi";
+$lotong="lotong";
+$shimen="shimen reservior";
+$dahsi="dahsi old street";
+$leofoo="leofoo village theme park";
+$sanyi="sanyi";
+$wuling="wuling farm";
+$fengjia="fengjia night market";
+$cingjing="cingjing";
+$lotus="lotus forest";
+$owanda="owanda";
+$sun="sun moon lake";
+$gukeng="gukeng green tunnel";
+$alishan="alishan";
+$guanziling="guanziling";
+$chimei="chimei museum";
+$anping="anping";
+$meinong="meinong";
+$tianliao="tianliao moon world";
+$lotus_pond="lotus pond";
+$formosa="formosa boulevard station";
+$love="love river";
+$sizihwan="sizihwan";
+$chijin="chijin";
+$kenting="kenting";
+$chihsingtan="chihsingtan";
+$chingshui="chingshui cliff";
+$hsiuguluan="hsiuguluan river";
+$chishang="Chishang";
+$taroko="Taroko National Park";
+$taimali="Taimali";
+$accommodation="Accommodation";
+$Fashionword="Fashionword";
+$food="food";
+$greeting="greeting";
+$hospital="hospital";
+$passerby="passerby";
+$shopping="shopping";
+$traffic="traffic";
+$weather="weather";
+if (strcasecmp($_POST['input'],$memorial_hall)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=ckmh.html>';
+
+}else if(strcasecmp($_POST['input'],$hsiangshan)==0 ){
+echo '<meta http-equiv=REFRESH CONTENT=1;url=hsiangshan.html>';
+
+}else if (strcasecmp($_POST['input'],$taipei101)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=101.html>';
+}else if (strcasecmp($_POST['input'],$beitou)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=beitou.html>';
+
+}else if (strcasecmp($_POST['input'],$yehliu)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=yehliu.html>';
+}else if (strcasecmp($_POST['input'],$jiufen)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=jiufen.html>';
+
+}else if (strcasecmp($_POST['input'],$taimsui)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=taimsiu.html>';
+
+}else if (strcasecmp($_POST['input'],$yangmingshan)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=ymmt.html>';
+
+}else if (strcasecmp($_POST['input'],$pingxi)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=pingxi.html>';
+}else if (strcasecmp($_POST['input'],$houtong)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=houtong.html>';
+}else if (strcasecmp($_POST['input'],$shilin)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=shilin.html>';
+}else if (strcasecmp($_POST['input'],$lotong)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=lotong.html>';
+
+}else if (strcasecmp($_POST['input'],$shimen)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=shimen.html>';
+
+}else if (strcasecmp($_POST['input'],$dahsi)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=daxi.html>';
+
+}else if (strcasecmp($_POST['input'],$leofoo)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=leofoo.html>';
+
+}else if (strcasecmp($_POST['input'],$sanyi)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=sanyi.html>';
+
+}else if (strcasecmp($_POST['input'],$wuling)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=wulingfarm.html>';
+}else if (strcasecmp($_POST['input'],$fengjia)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=fengjia.html>';
+}else if (strcasecmp($_POST['input'],$cingjing)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chingching.html>';
+
+}else if (strcasecmp($_POST['input'],$lotus)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=lotusforest.html>';
+
+}else if (strcasecmp($_POST['input'],$owanda)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=owenda.html>';
+}else if (strcasecmp($_POST['input'],$sun)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=sunmoonlake.html>';
+}else if (strcasecmp($_POST['input'],$gukeng)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=greentunnel.html>';
+
+}else if (strcasecmp($_POST['input'],$alishan)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=alishan.html>';
+
+}else if (strcasecmp($_POST['input'],$guanziling)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=guanziling.html>';
+
+}else if (strcasecmp($_POST['input'],$chimei)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chimei.html>';
+
+}else if (strcasecmp($_POST['input'],$anping)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=anping.html>';
+
+}else if (strcasecmp($_POST['input'],$meinong)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=meinong.html>';
+
+}else if (strcasecmp($_POST['input'],$tianliao)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=moonworld.html>';
+
+}else if (strcasecmp($_POST['input'],$lotus_pond)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=lotuspond.html>';
+
+}else if (strcasecmp($_POST['input'],$formosa)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=formosa.html>';
+
+}else if (strcasecmp($_POST['input'],$love)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=loveriver.html>';
+
+}else if (strcasecmp($_POST['input'],$sizihwan)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=sizihwan.html>';
+
+}else if (strcasecmp($_POST['input'],$chijin)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chijin.html>';
+
+}else if (strcasecmp($_POST['input'],$kenting)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=kenting.html>';
+
+}else if (strcasecmp($_POST['input'],$chihsingtan)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chihsingtan.html>';
+
+}else if (strcasecmp($_POST['input'],$chingshui)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chingshui.html>';
+
+}else if (strcasecmp($_POST['input'],$hsiuguluan)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=hsuiguluan.html>';
+
+}else if (strcasecmp($_POST['input'],$chishang)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=chishang.html>';
+
+}else if (strcasecmp($_POST['input'],$taroko)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=taroko.html>';
+
+}else if (strcasecmp($_POST['input'],$taimali)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=taimali.html>';
+}else if (strcasecmp($_POST['input'],$accommodation)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=dorm.html>';
+
+}else if (strcasecmp($_POST['input'],$Fashionword)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=fashionword.html>';
+}else if (strcasecmp($_POST['input'],$food)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=food.html>';
+
+}else if (strcasecmp($_POST['input'],$greeting)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=greeting.html>';
+}else if (strcasecmp($_POST['input'],$hospital)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=hospital.html>';
+
+}else if (strcasecmp($_POST['input'],$passerby)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=passerby.html>';
+
+}else if (strcasecmp($_POST['input'],$shopping)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=shopping.html>';
+
+}else if (strcasecmp($_POST['input'],$traffic)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=traffic.html>';
+
+}else if (strcasecmp($_POST['input'],$weather)==0 ) {
+echo '<meta http-equiv=REFRESH CONTENT=1;url=weather.html>';
+
+}else{
+	echo '<meta http-equiv=REFRESH CONTENT=1;url=error.html>';
+	
+}
+
+
+?>
